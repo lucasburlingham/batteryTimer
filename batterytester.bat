7536bee5dbcda32ca0c10f019210f56a27bc855a
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2020-03-02 15:14:08 -0500

    add batterytester.bat

---
+++ batterytester.bat
@@ -0,0 +1,16 @@
+@echo off
+mkdir %TEMP%\batteryTest\
+echo Made directory batteryTest at %TEMP%\batteryTest\
+set COUNT = 1
+echo ============ %date% ====== Started at: %time% ============ >> %TEMP%\batteryTest\batteryTest.log
+echo Log entry at %time% on %date%. >> %TEMP%\batteryTest\batteryTest.log
+rem Saved in "%TEMP%\batteryTest\
+echo Starting Battery Test >> %TEMP%\batteryTest\batteryTest.log
+echo Starting Battery Test at %time% on %date%.
+goto output
+:output
+echo Log entry at %time% on %date%, and is the %COUNT% iteration. >> %TEMP%\batteryTest\batteryTest.log
+echo >> %TEMP%\batteryTest\batteryTest.log
+timeout /t 120 /nobreak
+set /A Counter+=1
+goto:output
